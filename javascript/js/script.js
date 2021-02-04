61c4f8693cdd87612ba6c0ee9b273966c8ac06e6
melbacaicedo <melbacl1102@gmail.com>
2021-02-03 21:41:57 -0500

    [add] adicion de carpeta javascript

---
+++ javascript/js/script.js
@@ -0,0 +1,13 @@
+function hervirAgua(){
+        
+
+    for (var temperatura = 0; temperatura <= 100; temperatura++){
+     
+        console.log(temperatura + "grados")  
+       if (temperatura === 100){
+         alert("el agua ya esta hervida")
+     }
+    }
+
+
+}
